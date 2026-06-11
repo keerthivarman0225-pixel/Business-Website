440005b2c54ad7cd85ffc7f386fe104338a79adb
keerthivarman0225-pixel <keerthivarman0225@gmail.com>
2026-06-11 19:05:01 +0530

    Fix import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
-import Services from './components/services'
-import Projects from './components/projects'
-import Contact from './components/contact'
-import Footer from './components/footer'   
+import Services from './components/Services'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'  
 
 function App() {
   return (
